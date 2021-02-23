a4a811be66e3732ea20de5727f1adef686a6fcf9
Riley <rsbeach97@gmail.com>
2021-02-23 11:31:44 -0700

    updated userController to catch errors

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,25 +5,47 @@ const userController = {
   //get all users
   getAllUser(req, res) {
     User.find({})
+      .then(result => res.json(result))
+      .catch(err => res.status(400).json(err));
   },
   //get one user by id
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
+      .then(result => res.json(result))
+      .catch(err => res.status(400).json(err));
   },
   //create a new user
-  createUser({ body }, res) {
-    User.create(body)
+  createUser(req, res) {
+    User.create(req.body)
+      .then(result => res.json(result))
+      .catch(err => res.status(400).json(err));
   },
 
   //update user by id
   updateUser({ params, body }, res) {
     User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true})
+      .then(result => {
+        if (!result) {
+          res.status(400).json({ message: 'User not found'});
+          return;
+        }
+        res.json(result)
+      })
+      .catch(err => res.status(400).json(err));
   },
 
   //delete user
   deleteUser({ params, res }) {
     User.findOneAndDelete({ _id: params.id })
-  }
-}
+  .then(result => {
+        if (!result) {
+          res.status(400).json({ message: 'User not found'});
+          return;
+        }
+        res.json(result)
+      })
+      .catch(err => res.status(400).json(err));
+    }
+};
 
 module.exports = userController;
