b0e4b7e9287fcdfcadc0913ebd96d2574fedbcb0
Riley <rsbeach97@gmail.com>
2021-02-23 10:43:00 -0700

    created user model

---
+++ models/Thoughts.js
@@ -0,0 +1,21 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+
+    },
+
+    createdAt: {
+
+    },
+
+    username: {
+
+    },
+
+    reactions: {
+      
+    }
+  }
+)
