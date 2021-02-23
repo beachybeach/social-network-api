f33e480a330a6be0afa15127312126ad5466fcb4
Riley <rsbeach97@gmail.com>
2021-02-23 12:22:34 -0700

    update and delete thought

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,6 +1,7 @@
-const { Thoughts } = require('../models');
+const { Thoughts, User } = require('../models');
 
 const thoughtsController = {
+
   //find all thoughts
   getAllThoughts(req, res) {
     Thoughts.find({})
@@ -9,15 +10,61 @@ const thoughtsController = {
   },
 
   //find thought by id
-  getThoughtById({ params}, res) {
-    Thoughts.findOne({ _id: params.id})
-      .then(result => res.json(result))
+  getThoughtById({ params }, res) {
+    Thoughts.findOne({ _id: params.thoughtsId})
+      .then(result => {
+        if (!result) {
+          res.status(400).json({ message: 'Thought not found'});
+          return;
+        }
+        res.json(result)
+      })
       .catch(err => res.status(400).json(err));
   },
+
+
   //create new thought
-  createThought({ body }, res) {
-    Thoughts.create(body) 
-      .then(result => res.json(result))
+  createThought({ params, body }, res) {
+    Thoughts.create(body)
+    .then(({ _id }) => { 
+      return User.findOneAndUpdate(
+      {_id: params.userId},
+      { $push: { thoughts: _id } },
+      { new: true }
+      );
+    })
+  .then(result => {
+        if (!result) {
+          res.status(400).json({ message: 'User not found'});
+          return;
+        }
+        res.json(result)
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  updateThought({ params, body}, res) {
+    Thoughts.findOneAndUpdate({ _id: params.thoughts.Id }, body, {new: true, runValidators: true })
+     .then(result => {
+        if (!result) {
+          res.status(400).json({ message: 'User not found'});
+          return;
+        }
+        res.json(result)
+      })
+      .catch(err => res.status(400).json(err));
+
+    },
+
+  deleteThought({ params}, res) {
+    Thoughts.remove({ _id: params.thoughtsId })
+    .then(result => {
+        if (!result) {
+          res.status(400).json({ message: 'User not found'});
+          return;
+        }
+        res.json(result)
+      })
       .catch(err => res.status(400).json(err));
   }
-}
+};
